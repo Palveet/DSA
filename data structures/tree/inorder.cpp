0b1e0f81da78bc6c039adbf17bf4ef5fc155c5e2
Palveet <palveetkaursaluja@gmail.com>
2021-02-19 23:20:48 +0530

    inorder-traversal

---
+++ data structures/tree/inorder.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct node{
+    int data;
+    struct node* left,*right;
+    node(int data){
+        this->data=data;
+        left=right=NULL;
+    }
+    
+};
+void printInorder(struct node* node){
+    if(node==NULL)
+    return;
+    printInorder(node->left);
+    cout<<node->data<<" ";
+    printInorder(node->right);
+}
+int main(){
+    struct node *root = new node(1); 
+    root->left = new node(2); 
+    root->right = new node(3); 
+    root->left->left = new node(4); 
+    root->left->right = new node(5);
+    cout<<"INORDER TRAVERSAL:\n";
+    printInorder(root);
+    return 0;
+}
